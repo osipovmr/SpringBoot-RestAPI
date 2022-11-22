d7ad76c0f82231d985166e9df27ed7931f5fce88
osipovmr <osipovmr@yandex.ru>
2022-11-23 00:03:08 +0600

    добавил класс ролей

---
+++ src/main/java/com/cef/testTask/model/Role.java
@@ -0,0 +1,17 @@
+package com.cef.testTask.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+}
