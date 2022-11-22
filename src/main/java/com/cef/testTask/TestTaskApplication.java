79459865d38d364ae66809b3f01d61d50191cd72
osipovmr <osipovmr@yandex.ru>
2022-11-22 23:42:39 +0600

    выключил пароль при входе

--- src/main/java/com/cef/testTask/TestTaskApplication.java
+++ src/main/java/com/cef/testTask/TestTaskApplication.java
@@ -2,8 +2,9 @@ package com.cef.testTask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class TestTaskApplication {
 
 	public static void main(String[] args) {
